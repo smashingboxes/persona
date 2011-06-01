eb073d5d010b2f470cfccbe3166269b587c6cbdc
Nate Hunzaker <nate.hunzaker@gmail.com>
2011-06-01 10:03:34 -0400

    First git commit for Prototypical

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem "sinatra"
+gem "tilt"
+gem "pony"
+gem 'haml'
+gem 'compass'
---
+++ app.rb
@@ -0,0 +1,21 @@
+# Required Gems
+require "bundler/setup"
+
+require 'sinatra'
+require 'erb'
+# require 'pony' 
+
+
+# Routes
+get '/' do
+    erb :'index.html'
+end
+
+get '/:name' do 
+    erb :"#{params[:name]}.html"
+end
+
+
+get '/tools/type' do
+    erb :'type.html'
+end
