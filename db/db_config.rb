7572037a24f1e6fb9c57cd20eed8c5de6c9547bf
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-06-02 19:42:59 -0400

    more fixes..

--- db/db_config.rb
+++ db/db_config.rb
@@ -1,15 +1,7 @@
 require 'rubygems'
 require 'data_mapper'
 
-configure :development do
-  # A Sqlite3 connection to a persistent database
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/db/proto.db")
-end
-
-configure :production do 
-  # A Postgres connection:
-  DataMapper.setup(:default, 'postgres://kgvrhnbnbu:1AybmVA5tnxO7HkKOFI6@ec2-174-129-222-56.compute-1.amazonaws.com/kgvrhnbnbu')
-end
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/db/proto.db")
 
 class Page
   include DataMapper::Resource
