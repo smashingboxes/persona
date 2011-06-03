ccacb0df381184ab799041a99c5fcefe5e2bb951
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-06-02 22:41:43 -0400

    Updated JavaScript

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,3 +1,22 @@
 (function() {
-  $('.flash').delay(2000).fadeOut(750);
+  $('.flash').fadeTo(500, 0.95).delay(2500).fadeOut(500);
+  $('.flash').click(function() {
+    return $(this).stop().fadeOut(500);
+  });
+  $('#admin').hover(function() {
+    return $("#admin").stop().animate({
+      height: 130
+    }, 250);
+  }, function() {
+    return $("#admin").stop().animate({
+      height: 37
+    }, 250);
+  });
+  $('a.delete').click(function() {
+    var answer;
+    answer = confirm("Are you sure you want to delete this page?");
+    if (!answer) {
+      return false;
+    }
+  });
 }).call(this);
