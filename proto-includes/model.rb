97b157c96b3c6633ce21e904c21f4325ba92f9e3
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-06-15 12:57:36 -0400

    Clarified the database setup statement in model.rb

--- proto-includes/model.rb
+++ proto-includes/model.rb
@@ -4,7 +4,8 @@
 #                                       #
 #########################################
 
-require 'rubygems'
-require 'data_mapper'
+require_all 'rubygems', 'data_mapper'
 
+# We want to first check to see if there is an existing database on the server (in our case, Heroku)
+# If not, then default to a basic sqlite database
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/proto-includes/db/proto.db")
