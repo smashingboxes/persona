2a488cf4bfdcdce32435ba3e0df853304a1809e8
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-06-30 16:48:36 -0400

    Moved sqlite3 database to 'proto-includes'

--- proto-includes/model.rb
+++ proto-includes/model.rb
@@ -8,7 +8,7 @@ require_all 'rubygems', 'data_mapper'
 
 # We want to first check to see if there is an existing database on the server (in our case, Heroku)
 # If not, then default to a basic sqlite database
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/proto-includes/db/proto.db")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/proto-includes/proto.db")
 
 
 # Creates a datatable with the current website settings
