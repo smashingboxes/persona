83c322088d9c7c6114a96604801139635a28f10a
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-07-21 19:25:08 -0400

    More tweaks

--- personas/core/helpers.rb
+++ personas/core/helpers.rb
@@ -81,7 +81,8 @@
   #
   # Returns an action to render a template file
   def proto_genesis(filename="")       
-      erb :"#{look_for filename}"
+      erb :"themes/#{current_theme}/filename"
+      erb :"themes/default/filename"
   end
   
   
